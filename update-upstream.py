162fa7793cc47c28f1367169cc5b32e3e976e648
Changwoo Ryu <cwryu@debian.org>
2019-05-29 03:26:35 +0900

    업데이트
    - 한국어기초사전 2019년 5월 28일자 기준
    - 이전 다운로드와 차이점
      - 파일이 기존 2개에서 11개로 분리되었다.
      - 이전에 다운로드 XML이 invalid했던 원인 "큰따옴표" 설명이 바로잡힘
      - 새로 다국어 설명이 포함할 수 있게 되었는데 인도네시아어, 몽골어 설명
        부분에 새로 XML invalid 부분이 생겼다.
      - 단어를 강조하는 것 같은 포매팅, 예를 들어 "도박" 설명에 등장하는 "trump"를
        "</trump/>"와 같이 attribute 값으로 표기해 놓은 부분이 문제인데,
        이스케이프도 안 한 것도 잘못이고 웹페이지에서도 잘못된 태그이다.
    - 업데이트 스크립트에 해당 오류 수정 반영

--- update-upstream.py
+++ update-upstream.py
@@ -8,17 +8,47 @@ def do_extract(filename, outdir):
     zf = zipfile.ZipFile(filename)
     members = zf.namelist()
     members.sort()
-    found_xml_invalid = False
+    found_xml_invalid_ind = False
     for member, i in zip(members, range(0, len(members))):
         s = zf.open(member).read().decode('UTF-8')
-        if 'val="\'" "\'' in s:
+        if '</hanafoda/>' in s:
             print('Found XML format bug in %s. Correcting...' % member)
-            s = s.replace('val="\'" "\'', 'val="\'&quot; &quot;\'')
-            found_xml_invalid = True
+            s = s.replace('</hanafoda/>', 'hanafuda')
+            found_xml_invalid_ind = True
+        if '</trump/>' in s:
+            print('Found XML format bug in %s. Correcting...' % member)
+            s = s.replace('</trump/>', 'trump')
+            found_xml_invalid_ind = True
+        if '</Platycodon grandiflorus/>' in s:
+            print('Found XML format bug in %s. Correcting...' % member)
+            s = s.replace('</Platycodon grandiflorus/>',
+                          'Platycodon grandiflorus')
+            found_xml_invalid_ind = True
+        if '</bellflower/>' in s:
+            print('Found XML format bug in %s. Correcting...' % member)
+            s = s.replace('</bellflower/>', 'bellflower')
+            found_xml_invalid_ind = True
+        if '</sailfin sandfish/>' in s:
+            print('Found XML format bug in %s. Correcting...' % member)
+            s = s.replace('</sailfin sandfish/>', 'sailfin sandfish')
+            found_xml_invalid_ind = True
+        if 'val="<i>perilla</i>, biji <i>perilla</i>"' in s:
+            print('Found XML format bug in %s. Correcting...' % member)
+            s = s.replace('val="<i>perilla</i>, biji <i>perilla</i>"',
+                          'val="perilla, biji perilla"')
+            found_xml_invalid_ind = True
+        if 'val="Их хаан<BR/>' in s:
+            print('Found XML format bug in %s. Correcting...' % member)
+            s = s.replace('val="Их хаан<BR/>', 'val="Их хаан&#xA;')
+            found_xml_invalid_mon = True
         with open(os.path.join(outdir, str(i) + '.xml'), 'w') as fp:
             fp.write(s)
-    if not found_xml_invalid:
-        print('WARNING: XML invalid not found. Check if upstream fixed it.')
+    if not found_xml_invalid_ind:
+        print('WARNING: XML invalid on indonesian not found. ' +
+              'Check if upstream fixed it.')
+    if not found_xml_invalid_mon:
+        print('WARNING: XML invalid on mongolian not found. ' +
+              'Check if upstream fixed it.')
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
